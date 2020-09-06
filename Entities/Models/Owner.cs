65da4698eec694a930060ac32058452b778c8cdd
Vien Pham <phamthevien@archway.co.jp>
2020-09-06 11:47:21 +0700

    feat: add classlib Entities, Owner, and Account models in Models

---
+++ Entities/Models/Owner.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+	[Table("owners")]
+	public class Owner
+	{
+		public Guid OwnerId { get; set; }
+
+		[Required(ErrorMessage = "Name is required")]
+		[StringLength(60, ErrorMessage = "Name can't be longer than 60 characters")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Date of birth is required")]
+		public DateTime DateOfBirth { get; set; }
+
+		[Required(ErrorMessage = "Address is required")]
+		[StringLength(100, ErrorMessage = "Address can't be longer than 100 characters'")]
+		public string Address { get; set; }
+
+		public ICollection<Account> Accounts { get; set; }
+	}
+}
